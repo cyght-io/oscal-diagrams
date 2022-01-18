6d4b646389d535e09f239f9fcec33d194f06994d
Guy Zylberberg <guyzyl@gmail.com>
2022-01-18 19:42:18 +0200

    Create generate_diagrams.sh

---
+++ generate_diagrams.sh
@@ -0,0 +1,8 @@
+# Generate diagrams for all models
+erdantic trestle.oscal.assessment_plan.AssessmentPlan -o ./diagrams/AssessmentPlan.png
+erdantic trestle.oscal.assessment_results.AssessmentResults -o ./diagrams/AssessmentResults.png
+erdantic trestle.oscal.catalog.Catalog -o ./diagrams/Catalog.png
+erdantic trestle.oscal.component.ComponentDefinition -o ./diagrams/ComponentDefinition.png
+erdantic trestle.oscal.poam.PlanOfActionAndMilestones -o ./diagrams/PlanOfActionAndMilestones.png
+erdantic trestle.oscal.profile.Profile -o ./diagrams/Profile.png
+erdantic trestle.oscal.ssp.SystemSecurityPlan -o ./diagrams/SystemSecurityPlan.png
